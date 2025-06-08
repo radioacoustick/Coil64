5e9dd7ac6446b3e97d066d468590130509254e0f
radioacoustick <46995068+radioacoustick@users.noreply.github.com>
2025-06-08 14:58:56 +0300

    Update multisection.h

--- multisection.h
+++ multisection.h
@@ -1,4 +1,21 @@
-﻿#ifndef MULTISECTION_H
+/* multisection.h - header text to Coil64 - Radio frequency inductor and choke calculator
+Copyright (C) 2025 Kustarev V.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses
+*/
+
+#ifndef MULTISECTION_H
 #define MULTISECTION_H
 
 #include <QDialog>
